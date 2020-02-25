ed9693f755335a4e54a9533fbc0a333372435c44
Dmitrij Belousov <dmitrij.belousov@gmail.com>
2020-02-25 21:35:16 +0100

    add some debug output

--- main.cpp
+++ main.cpp
@@ -75,7 +75,7 @@ static int bluez_agent_call_method(const gchar *method, GVariant *param)
                                          G_DBUS_CALL_FLAGS_NONE,
                                          -1,
                                          nullptr,
-                                        &error);
+                                         &error);
     if(error != nullptr) {
         g_print("Register %s: %s\n", AGENT_PATH, error->message);
         return 1;
@@ -103,9 +103,9 @@ static int bluez_register_autopair_agent(const char *cap)
 }
 
 static void bluez_signal_device_changed(GDBusConnection *conn,
-                                        const gchar *sender,
+                                        const gchar */*sender*/,
                                         const gchar *path,
-                                        const gchar *interface,
+                                        const gchar */*interface*/,
                                         const gchar *signal,
                                         GVariant *params,
                                         void */*userdata*/)
@@ -117,27 +117,32 @@ static void bluez_signal_device_changed(GDBusConnection *conn,
     GVariant *value = nullptr;
     const gchar *signature = g_variant_get_type_string(params);
 
+    g_print("org.bluez.Device1 : %s\n", path);
+
     if(strcmp(signature, "(sa{sv}as)") != 0) {
         g_print("Invalid signature for %s: %s != %s", signal, signature, "(sa{sv}as)");
         goto done;
     }
-    g_print("sender: %s\n",sender);
-    g_print("path: %s\n",path);
-    g_print("interface: %s\n",interface);
+
     g_variant_get(params, "(&sa{sv}as)", &iface, &properties, &unknown);
     while(g_variant_iter_next(properties, "{&sv}", &key, &value)) {
-        g_print("Property: %s\n",key);
+        g_print("\tProperty: %s\n",key);
+        if(g_variant_is_of_type(value, G_VARIANT_TYPE_BOOLEAN)) {
+            g_print("\tValue :\"%s\"\n", g_variant_get_boolean(value) ? "true" : "false");
+        }
+
         if(!g_strcmp0(key, "Connected")) {
             if(!g_variant_is_of_type(value, G_VARIANT_TYPE_BOOLEAN)) {
                 g_print("Invalid argument type for %s: %s != %s", key,
                         g_variant_get_type_string(value), "b");
                 goto done;
             }
-            g_print("Device is \"%s\"\n", g_variant_get_boolean(value) ? "Connected" : "Disconnected");
             if(g_variant_get_boolean(value)){
                 DeviceClass d(conn,path);
                 d.SetTrusted(true);
             }
+
+
         }
     }
 done:
@@ -147,6 +152,41 @@ done:
         g_variant_unref(value);
 }
 
+static void bluez_adapter_changed(GDBusConnection */*conn*/,
+                                  const gchar */*sender*/,
+                                  const gchar *path,
+                                  const gchar */*interface*/,
+                                  const gchar *signal,
+                                  GVariant *params,
+                                  void */*userdata*/)
+{
+    GVariantIter *properties = nullptr;
+    GVariantIter *unknown = nullptr;
+    const char *iface;
+    const char *key;
+    GVariant *value = nullptr;
+    const gchar *signature = g_variant_get_type_string(params);
+
+    g_print("org.bluez.Adapter1 : %s\n", path);
+
+    if(strcmp(signature, "(sa{sv}as)") != 0) {
+        g_print("Invalid signature for %s: %s != %s", signal, signature, "(sa{sv}as)");
+        goto done;
+    }
+
+    g_variant_get(params, "(&sa{sv}as)", &iface, &properties, &unknown);
+    while(g_variant_iter_next(properties, "{&sv}", &key, &value)) {
+        g_print("\tProperty: %s\n",key);
+        if(g_variant_is_of_type(value, G_VARIANT_TYPE_BOOLEAN)) {
+            g_print("\tValue :\"%s\"\n", g_variant_get_boolean(value) ? "true" : "false");
+        }
+    }
+done:
+    if(properties != nullptr)
+        g_variant_iter_free(properties);
+    if(value != nullptr)
+        g_variant_unref(value);
+}
 
 int main(void)
 {
@@ -188,6 +228,17 @@ int main(void)
                                        "org.bluez.Device1",
                                        G_DBUS_SIGNAL_FLAGS_NONE,
                                        bluez_signal_device_changed,
+                                       nullptr,
+                                       nullptr);
+
+    g_dbus_connection_signal_subscribe(con,
+                                       "org.bluez",
+                                       "org.freedesktop.DBus.Properties",
+                                       "PropertiesChanged",
+                                       nullptr,
+                                       "org.bluez.Adapter1",
+                                       G_DBUS_SIGNAL_FLAGS_NONE,
+                                       bluez_adapter_changed,
                                        nullptr,
                                        nullptr);
 
